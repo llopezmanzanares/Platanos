38b79d3d2d20ddc2d0f8a7e92a4649aed8841fd8
llopezmanzanares <luislmf@gmail.com>
2021-08-16 17:41:07 +0100

    Lectura y pre procesado de datos de las toneladas importadas desde Tenerife

---
+++ eda/lee_importaciones.R
@@ -0,0 +1,19 @@
+library(tidyverse)
+
+ds <- read_delim("data/raw/C00012A_0001.txt",
+                 skip = 7,
+                 col_names = c("fecha", "pen", "extr"),
+                 show_col_types = F,
+                 trim_ws = T) %>% 
+  filter(
+    fecha != "Tenerife",
+    pen > 0
+  ) %>% 
+  mutate(
+    fecha = str_c(str_extract(fecha, "\\d+"),  # saco el año
+                  str_extract(fecha, "[A-Z][a-z]+"), #saco el mes, sin (p)
+                  sep = " "),
+    .before = 1
+  ) %>% 
+  write_csv("data/processed/imp_tfe.csv")
+
