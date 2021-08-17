a4fc86bf219ce91dda3d7287aca19abbe5aff636
llopezmanzanares <luislmf@gmail.com>
2021-08-17 17:47:20 +0100

    Intento modificar el formato de la fecha

--- eda/lee_importaciones.R
+++ eda/lee_importaciones.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(lubridate)
 
 # Los datos del último año son provisionales y vienen marcadas las fechas
 # con un (p) que es necesario eliminar (líneas 16 y 17)
@@ -13,9 +14,14 @@ ds <- read_delim("data/raw/C00012A_0001.txt",
     pen > 0
   ) %>% 
   mutate(
-    fecha = str_c(str_extract(fecha, "\\d+"),  # saco el año
-                  str_extract(fecha, "[A-Z][a-z]+"), #saco el mes, sin (p)
-                  sep = " "),
+    año = str_extract(fecha, "\\d+"),
+    mes = str_extract(fecha, "[A-Z][a-z]+"),
+    fecha = str_c("01", mes, año, sep = "-"), #esto no me funciona
+    mes1 = month(fecha),
+    # fecha = str_c(str_extract(fecha, "\\d+"),  # saco el año
+    #               str_extract(fecha, "[A-Z][a-z]+"), #saco el mes, sin (p)
+    #               sep = " "),
+    # fecha = guess_formats(fecha, "Y B"),
     .before = 1
   ) %>% 
   write_csv("data/processed/imp_tfe.csv")
