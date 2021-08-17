53b15f7a04ad620c75b94f814a34c373c504db1e
llopezmanzanares <luislmf@gmail.com>
2021-08-17 16:56:46 +0100

    Modificado cómo se guarda el tiempo en el archivo. Una columna para el año y otra para el número de semana

--- eda/lee_precios.R
+++ eda/lee_precios.R
@@ -10,6 +10,13 @@ ds <- read_delim("data/raw/dataset-ISTAC-C00014A_000012-1.3-observations.tsv",
   ) %>% 
   select(1,6) %>% 
   rename(
+    fecha = time_period,
     pmp = obs_value
-  ) %>% 
+  ) %>%
+  mutate(
+    año = str_extract(fecha, "\\d+"),
+    sem = str_extract(fecha, "\\d+$"),
+    .before = 1
+  ) %>%
+  select(-3) %>% 
   write_csv("data/processed/pr_tfe.csv")
