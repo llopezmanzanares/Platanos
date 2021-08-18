cedec07bc022dc3e4808c5645cda58fabda16646
llopezmanzanares <luislmf@gmail.com>
2021-08-18 15:55:52 +0100

    Uno los datos mensuales de producción y precios medios ponderados, para su análisis

--- eda/join_imp_pre_m.R
+++ eda/join_imp_pre_m.R
@@ -1,10 +1,34 @@
 library(tidyverse)
-library(lubridate)
 
 imp <- read_csv("data/processed/imp_tfe.csv",
-                show_col_types = F) %>% 
-  mutate(
-    fecha = as.Date(fecha, tryformats = "%d-%m-%Y")
-  )
+                show_col_types = F)
+
 pr_m <- read_csv("data/processed/pr_tfe_mes.csv",
                  show_col_types = F)
+
+mes <- imp %>% 
+  left_join(pr_m, by = "fecha") %>% 
+  mutate(
+    fecha = as.Date(fecha),
+    pd_tot = pen + extr
+  ) 
+
+meses <- mes %>% 
+  pivot_longer(
+    !fecha,
+    names_to = "tipo",
+    values_to = "valor"
+  )
+
+ggplot(mes, aes(fecha, pmp)) +
+  geom_line()
+
+meses %>% 
+  filter(
+    tipo %in% c("pmp", "pd_tot")
+  ) %>% 
+ggplot(aes(fecha, valor)) +
+  geom_line() +
+  facet_wrap(~tipo, 
+             ncol = 1,
+             scales = "free_y")
