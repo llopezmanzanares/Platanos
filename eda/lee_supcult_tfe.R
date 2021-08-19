130418edc58592686b811de2527818ea4578b228
llopezmanzanares <luislmf@gmail.com>
2021-08-19 18:05:13 +0100

    Corregida la lectura de los datos de superficie

--- eda/lee_supcult_tfe.R
+++ eda/lee_supcult_tfe.R
@@ -1,11 +1,13 @@
 library(tidyverse)
 
-ds <- read_delim("data/raw/C00013A_0014.txt",
-                 skip = 9,
-                 col_names = c("anualidad", "secano", "regadio"),
-                 col_types = "ccc",
-                 col_select = c(1,3)) %>% 
-  filter(
-    !is.na(regadio)
+ds <- read_csv("data/raw/C00013A_0014.txt",
+                 skip = 7,
+                 col_names = c("anualidad", "hectareas")
+                 ) %>% 
+   mutate(
+    anualidad = as.numeric(anualidad)
+  ) %>% 
+ filter(
+    !is.na(anualidad)
   ) %>% 
   write_csv("data/processed/supcult_tfe.csv")
