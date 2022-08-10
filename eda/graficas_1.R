8e865024d8c775c74bc7789628d8a66b4a9934e3
llopezmanzanares <luislmf@gmail.com>
2022-08-10 23:16:28 +0100

    Analisis de precios. Evolución temporal

--- eda/graficas_1.R
+++ eda/graficas_1.R
@@ -1,10 +1,21 @@
 # Análisis de la temporalidad de los precios
 
 library(tidyverse)
+library(here)
+
+if (file.exists(here("data/processed", "platanos.RData")))
+  load(here("data/processed", "platanos.RData"))
+
 
 precios_sem %>% 
-  filter(territorio == "Tenerife") %>% 
-  ggplot(aes(semana, precio, color = lubridate::year(semana))) +
-  geom_smooth(se = F, span = 0.8) +
+  mutate(
+    sem = str_extract(periodo, pattern = "\\d{2}$") %>% 
+      tolower() %>% 
+      as.numeric(),
+    .after = 1
+  ) %>% 
+  filter(territorio == "tenerife") %>% 
+  ggplot(aes(sem, precio, color = as.factor(anualidad))) +
+  # geom_smooth(se = F, span = 0.8) +
   geom_line() +
   theme_light()
