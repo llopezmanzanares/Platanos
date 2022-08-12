1f4ddd18be0dad09d17f430a8b1ab6aa353ec933
llopezmanzanares <luislmf@gmail.com>
2022-08-12 16:35:56 +0100

    Gráficas con los datos de las exportaciones

---
+++ eda/exportaciones_graficas.R
@@ -0,0 +1,43 @@
+# Análisis de la temporalidad de las exportaciones
+
+# Exportaciones por islas -------------------------------------------------
+
+
+exportaciones %>% 
+  filter(isla != "Canarias") %>% 
+  ggplot(aes(mes, total, color = isla)) +
+  geom_line(size = 1) +
+  labs(
+    title = "Exportaciones de plátanos, Tn",
+    y = NULL,
+    x = NULL
+  ) +
+  scale_y_continuous(position = "right") +
+  theme_light()
+
+ggsave(
+  here("reports/graph", "expor_islas.pdf")
+)
+
+
+# Exportaciones Tenerife --------------------------------------------------
+
+exportaciones %>% 
+  filter(isla == "Tenerife") %>% 
+  select(mes, peninsula, extranjero) %>% 
+  pivot_longer(
+    !mes,
+    names_to = "destino",
+    values_to = "tn"
+  ) %>% 
+  ggplot(aes(mes, tn, color = destino)) +
+  geom_line(size = 1) +
+  labs(
+    title = "Exportaciones de plátanos. Tn",
+    subtitle = "Isla de Tenerife",
+    x = NULL,
+    y = NULL
+  ) +
+  theme_light()
+    
+
