167f219d5ef7ffadbb77df697e9e0699a6e06101
llopezmanzanares <luislmf@gmail.com>
2022-08-13 16:40:29 +0100

    Gráfica de producción anual por isla

--- eda/tn_graficas.R
+++ eda/tn_graficas.R
@@ -6,6 +6,21 @@
 Tn <- toneladas %>% 
   filter(territorio != "canarias") %>% 
   mutate(
+    anualidad = as.numeric(anualidad),
     territorio = str_replace(territorio, pattern = "\\.", replacement = " ") %>% 
       str_to_title()
   )
+
+# Tn todas las islas ------------------------------------------------------
+
+Tn %>% 
+  ggplot(aes(anualidad, tn, color = territorio)) +
+  geom_line(size = 1) +
+  labs(
+    title = "Producción agrícola por islas, Tn",
+    x = NULL,
+    y = NULL,
+    color = "Isla"
+  ) +
+  scale_y_continuous(position = "right") +
+  theme_light()
