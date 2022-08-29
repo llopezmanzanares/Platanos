938b0235a7a0da92d940aa2185b51b3a87f7cd61
llopezmanzanares <luislmf@gmail.com>
2022-08-29 14:13:55 +0100

    Lectura de los datos de proporcionados por la cooperativa

---
+++ code/get_coop_data.R
@@ -0,0 +1,18 @@
+# datos de producción proporcionados por la cooperativa
+
+library(tidyverse)
+library(lubridate)
+library(pdftools)
+library(here)
+
+data_files <- list.files(
+  path = here("data/raw", "coop"),
+  pattern = "^L"
+  )
+
+ds <- 
+  map(here("data/raw", "coop", data_files), pdf_text) %>% 
+  unlist() %>% 
+  str_split(pattern = "\\n") %>% 
+  unlist() %>% 
+  as_tibble()
