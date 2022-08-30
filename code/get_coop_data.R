fdfaf41f51f245c14a742cabe4cc331144cb4d2c
L. Lopez-Manzanares <63161257+llopezmanzanares@users.noreply.github.com>
2022-08-30 14:43:45 +0100

    Pivot_wider no es la solución
    Extraigo los diferentes subconjuntos de datos, opero con ellos y luego los vuelvo a unir

--- code/get_coop_data.R
+++ code/get_coop_data.R
@@ -7,6 +7,11 @@ library(here)
 
 # Constantes --------------------------------------------------------------
 
+dir <- list(
+  raw = "data/raw",
+  pro = "data/processed"
+)
+
 patrones <- list(
   kg    = "\\d+",              # es un número entero de varias cifras
   prc   = "\\d{1,2},\\d{1,2}", # número de 2 decimales
@@ -26,13 +31,12 @@ xtr_num <- function(txt, patron){
 # Leo y transformo --------------------------------------------------------
 
 data_files <- list.files(
-  path = here("data/raw", "coop"),
+  path = here(dir%raw, "coop"),
   pattern = "^L"
   )
 
 ds <- 
-  map(here("data/raw", "coop", data_files), pdf_text) %>% 
-  unlist() %>% 
+  map(here(dir$raw, "coop", data_files), pdf_text) %>% 
   str_split(pattern = "\\n") %>% 
   unlist() %>% 
   as_tibble() %>% 
@@ -63,26 +67,81 @@ ds <-
     # categoría premium
     premium_kg =    xtr_num(premium, patrones$kg),
     premium_pc =    xtr_num(premium, patrones$prc),
-    premium_eurkg = xtr_num(premium, patrones$eurkg),
-    premium_eur =   xtr_num(premium, patrones$eur),
-    # categoría superior
-    psup_kg =    xtr_num(psup, patrones$kg),
-    psup_pc =    xtr_num(psup, patrones$prc),
-    psup_eurkg = xtr_num(psup, patrones$eurkg),
-    psup_eur =   xtr_num(psup, patrones$eur),
-    # categoría segunda
-    segunda_kg =    xtr_num(segunda, patrones$kg),
-    segunda_pc =    xtr_num(segunda, patrones$prc),
-    segunda_eurkg = xtr_num(segunda, patrones$eurkg),
-    segunda_eur =   xtr_num(segunda, patrones$eur),
-    .keep = "unused"
+    premium_eurkg = xtr_num(premium, patrones$eurkg),ds <- 
+  map(
+    here(dir$raw, data_files),
+    pdf_text
+  ) %>% 
+  str_split(pattern = "\\n") %>% 
+  unlist() %>% 
+  as_tibble() %>% 
+  filter(str_detect(value, pattern = "Fecha|Semana|PREMIUM|P\\. SUPER|SEGUNDA|racimos|medio")) %>% 
+  mutate(
+    value  = str_to_lower(value),
+    medida = str_extract(value, pattern = "[:graph:]+")
+  ) 
+
+# voy extrayendo los subconjuntos de datos
+fechas <- filter(ds, medida == "fecha") %>% 
+  mutate(
+    fecha = str_extract(value, pattern = "(\\d{2}\\.?){3}") %>% 
+      str_replace(pattern = "\\.", replacement = "-") %>% 
+      dmy(),
+    .keep = "none"
+  )
+semanas <- filter(ds, medida == "semana") %>% 
+  mutate(
+    semana = xtr_num(value, "\\d{1,2}"),
+    .keep = "none"
+  )
+racimos <- filter(ds, medida == "total") %>% 
+  mutate(
+    racimos = xtr_num(value, "\\d{1,2}"),
+    .keep = "none"
+  )
+peso_med <- filter(ds, medida == "peso") %>% 
+  mutate(
+    peso_med = xtr_num(value, "\\d+,\\d{2}"),
+    .keep = "none"
+  )
+prec_med <- filter(ds, medida == "precio") %>% 
+  mutate(
+    prec_med = xtr_num(value, "\\d,\\d{1,4}"),
+    .keep = "none"
+  )
+premium <- filter(ds, medida == "premium") %>% 
+  mutate(
+    premium_kg =    xtr_num(value, patrones$kg),
+    premium_pc =    xtr_num(value, patrones$prc),
+    premium_eurkg = xtr_num(value, patrones$eurkg),
+    premium_eur =   xtr_num(value, patrones$eur),
+    .keep = "none"
+  )
+psup <- filter(ds, medida == "p.") %>% 
+  mutate(
+    psup_kg =    xtr_num(value, patrones$kg),
+    psup_pc =    xtr_num(value, patrones$prc),
+    psup_eurkg = xtr_num(value, patrones$eurkg),
+    psup_eur =   xtr_num(value, patrones$eur),
+    .keep = "none"
+  )
+segunda <- filter(ds, medida == "segunda") %>% 
+  mutate(
+    segunda_kg =    xtr_num(value, patrones$kg),
+    segunda_pc =    xtr_num(value, patrones$prc),
+    segunda_eurkg = xtr_num(value, patrones$eurkg),
+    segunda_eur =   xtr_num(value, patrones$eur),
+    .keep = "none"
   )
 
-cosa <- ds %>% 
-  filter(
-    str_detect(value, "Semana|Fecha|PREMIUM|P. SU|SEGUNDA")
-    ) %>% 
-  mutate(
-    medida1 = str_extract(value, pattern = "^[:graph:]+"),
-    .before = 1
-  )
+
+datos <- bind_cols(fechas, semanas, racimos, peso_med, prec_med, premium, psup, segunda)
+
+rm(ds, fechas, semanas, racimos, peso_med, prec_med, premium, psup, segunda)
+
+
+# Guardo el conjunto de datos ---------------------------------------------
+
+save(datos, file = here(dir$pro, "datos_finca.RData"))
+
+write_csv2(datos, file = here(dir$pro, "datos_finca.csv"))
