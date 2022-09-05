f8adfc032985fd8866fde4f55933894c4a20928b
llopezmanzanares <luislmf@gmail.com>
2022-09-05 13:12:01 +0100

    Update coop_data.R Cálculo de agrupaciones mensuales

--- eda/coop_data.R
+++ eda/coop_data.R
@@ -5,3 +5,16 @@ library(here)
 
 load(here("data/processed", "datos_finca.RData"))
 
+# para poder agrupar por meses me llevo las fechas al último día de cada mes
+datos_sem <- datos_sem %>% 
+  mutate(
+    mes = lubridate::rollforward(fecha),
+    .after = 1
+  )
+
+datos_mes <- datos_sem %>% 
+  group_by(mes) %>% 
+  summarise(
+    across(racimos:segunda_eur, ~sum(.x, na.rm = T))
+  ) %>% 
+  select(!ends_with("_pc"))
