ce480b3db0e90257e0143405f24d36a8cf33a23e
llopezmanzanares <luislmf@gmail.com>
2022-09-05 14:03:44 +0100

    Update coop_data.R Simplificado el código del script Añadido el guardado de los datos

--- eda/coop_data.R
+++ eda/coop_data.R
@@ -6,15 +6,19 @@ library(here)
 load(here("data/processed", "datos_finca.RData"))
 
 # para poder agrupar por meses me llevo las fechas al último día de cada mes
-datos_sem <- datos_sem %>% 
+datos_mes <- datos_sem %>% 
   mutate(
     mes = lubridate::rollforward(fecha),
     .after = 1
-  )
-
-datos_mes <- datos_sem %>% 
+  ) %>% 
   group_by(mes) %>% 
   summarise(
     across(racimos:segunda_eur, ~sum(.x, na.rm = T))
   ) %>% 
   select(!ends_with(c("_pc", "_eurkg", "prec_med")))
+
+# guardo los datos
+
+save(datos_mes, datos_sem, file = here("data/processed", "ds_cooperativa.RData"))
+
+write_csv2(datos_mes, file = here("data/processed", "ds_coop_mes.csv"))
