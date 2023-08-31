0d234e25dec24b0991379cbfcdf2bcc271f9df8d
Luis Ovidio Lopez-Manzanares Fernandez <luislmf@gmail.com>
2023-08-31 13:17:55 +0100

    Genero el código para usar el script de lectura de los pdf y generar coop_ds$sem que almaceno como datos_finca.RData

--- code/finca_eda.R
+++ code/finca_eda.R
@@ -3,3 +3,25 @@
 
 # Versión: 2023-08-31
 
+# Packages ------
+
+library(tidyverse)
+# library(lubridate)
+# library(pdftools)
+library(here)
+
+# Constantes --------------------------------------------------------------
+
+coop_ds <- list()
+
+dirs <- list(
+  raw = "data/raw",
+  cop = "data/raw/coop",
+  pro = "data/processed",
+  cod = "code"
+)
+
+
+# Functions ---------------------------------------------------------------
+
+source(here(dirs$cod, "get_coop_data.R"))
