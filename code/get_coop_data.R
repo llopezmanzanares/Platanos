db6dda97819ce4d76d1b015af1de8d3fed71f1d3
Luis Ovidio Lopez-Manzanares Fernandez <luislmf@gmail.com>
2023-08-31 13:25:13 +0100

    Añado el cálculo de los acumulados mensuales y de los kg mensuales

--- code/get_coop_data.R
+++ code/get_coop_data.R
@@ -147,10 +147,50 @@ data_files <- list.files(
 
 coop_ds$sem <- read_pdfs(data_files) %>% xtr_sem_data()
 
-# agregados mensuales y anuales
+# Agregados mensuales -----------------------------------------------------
+
+# Es más útil trabajar con los datos mensuales
+# solo tengo un dato de 2020, así que lo elimino
+coop_ds$mes <- 
+  filter(coop_ds$sem, year(fecha) > 2020) %>% 
+  select(
+    !c(semana,             # el dato de la semana no aporta información
+       total_fac,          # el total facturado es muy similar al importe, lo quito
+       ends_with(c("_med", # tampoco las medias y porcentajes
+                   "_pc",
+                   "_eurkg")
+       )
+    )
+  ) %>% 
+  mutate(
+    fecha = rollforward(fecha)
+  ) %>% 
+  group_by(fecha) %>% 
+  summarise(
+    across(everything(), sum),
+    .groups = "drop"
+  ) %>% 
+  mutate(
+    kg_rac = total_kg / racimos
+  )
+
+# Kg por meses ------------------------------------------------------------
+
+coop_ds$mes_kg <- coop_ds$mes %>% 
+  mutate(
+    aa = year(fecha),
+    mm = month(fecha, label = TRUE),
+    .after = 1
+  ) %>% 
+  select(fecha:mm, ends_with("kg")) %>% 
+  group_by(aa) %>% 
+  mutate(
+    across(ends_with("_kg"), cumsum, .names = "{.col}_acum")
+  ) %>% 
+  ungroup()
 
 # Guardo el conjunto de datos ---------------------------------------------
 
-save(coop_ds, file = here(dirs$pro, "datos_finca.RData"))
+save(coop_ds, file = here(dirs$pro, "datos_finca_test.RData"))
 
 # write_csv2(coop_ds$sem, file = here(dirs$pro, "datos_finca.csv"))
