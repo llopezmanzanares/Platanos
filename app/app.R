b999cc54b5fd94558f82b376df27e9ca08aef8ad
Luis Ovidio Lopez-Manzanares Fernandez <luislmf@gmail.com>
2025-05-19 11:34:25 +0100

    Stylized the code Old and useless version

--- app/app.R
+++ app/app.R
@@ -2,28 +2,28 @@ library(tidyverse)
 library(here)
 library(shiny)
 
+# version: 2025-04-14
+
+# Esto viene de un tiempo anterior Abril 2023
+# Eliminar el código e intentar hacer algo similar al report
 
 # Carga de datos  ---------------------------------------------------------
 
+load(here("data/processed/datos_coop_liq.RData"))
 # load(file = here("data/processed/datos_finca.RData"))
 # cats <- c("Premium", "P Sup", "Segunda")
 
 # UI  ---------------------------------------------------------------------
 ui <- fluidPage(
-  
   titlePanel("Una historia de plátanos"),
-  
   sidebarLayout(
-    
     sidebarPanel(
-      
       selectInput("categoria",
-                  label    = "Selecciona una categoría:",
-                  choices  = c("Premium", "P Sup", "Segunda"),
-                  selected = "Premium"
+        label    = "Selecciona una categoría:",
+        choices  = c("Premium", "P Sup", "Segunda"),
+        selected = "Premium"
       )
     ),
-    
     mainPanel(
       textOutput("selected_categoria")
     )
@@ -33,11 +33,10 @@ ui <- fluidPage(
 
 # Servidor  ---------------------------------------------------------------
 
-server <- function(input, ouput){
-  
+server <- function(input, ouput) {
   output$selected_categoria <- renderText({
-      paste("La selección es", input$categoria)
-      })
+    paste("La selección es", input$categoria)
+  })
 }
 
 
