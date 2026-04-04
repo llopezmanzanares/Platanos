85820b6e431b76febd5b94469344acc55a0af570
Luis Ovidio Lopez-Manzanares Fernandez <luislmf@gmail.com>
2026-04-04 19:05:34 +0100

    Limpieza de código. Se va a sustituir el script

--- code/get_balten_data.R
+++ code/get_balten_data.R
@@ -7,55 +7,61 @@
 
 library(pdftools)
 
-balten_ds  <- list()
+balten_ds <- list()
 
 # Funciones ---------------------------------------------------------------
 
 # Extraigo un número no entero y lo transformo de cadena de texto a número
-xtr_cifra <- function(txt, patron){
-  numero <- str_extract(txt, patron) %>% 
-    str_replace("\\.", "") |>    # en el caso de que sea > 1000 tiene un punto
-    str_replace(",", ".") |>  
+xtr_cifra <- function(txt, patron) {
+  numero <- str_extract(txt, patron) |>
+    str_remove("\\.") |> # en el caso de que sea > 1000 tiene un punto
+    str_replace(",", ".") |> # normaliza decimal
     as.numeric()
-  
+
   return(numero)
 }
 
-balten_files <- list.files(path = here::here(dirs$bal))
+balten_files <- list.files(path = here(dirs$bal), full.names = TRUE)
 
-balten <- 
-  map(here::here(dirs$bal, balten_files), pdf_text) |> 
-  unlist() |> 
-  str_split(pattern = "\\n") |> 
-  unlist() |> 
-  as_tibble_col() |> 
-  filter(str_detect(value, pattern = "Lectura Ant|Agua Re"))
+# Forma previa
+# balten <-
+#   map(balten_files, pdf_text) |>
+#   unlist() |>
+#   str_split(pattern = "\\n") |>
+#   unlist() |>
+#   as_tibble_col() |>
+#   filter(str_detect(value, pattern = "Lectura Ant|Agua Re"))
 
-balten_ds$todos <- 
-  balten |> 
+balten_raw <- map(balten_files, pdf_text) |>
+  set_names(basename(balten_files)) |>
+  enframe(name = "archivo", value = "contenido") |>
+  mutate(lineas = map(contenido, ~ str_split(.x, "\\n")[[1]])) |>
+  unnest(lineas) |>
+  filter(str_detect(lineas, pattern = "Lectura Ant|Agua Re"))
+
+
+balten_ds$todos <- balten_raw |>
   mutate(
     # lectura anterior del contador, y fecha
-    l_ant = str_extract(value, pattern = "(?<=Lectura Anterior: )\\d+") |> as.numeric(),
-    f_ant = str_extract(value, pattern = "(?<=Fecha: )(\\d{2}/){2}\\d{4}") |> dmy(),
+    l_ant = as.numeric(str_extract(lineas, pattern = "(?<=Lectura Anterior: )\\d+")),
+    f_ant = dmy(str_extract(lineas, pattern = "(?<=Fecha: )(\\d{2}/){2}\\d{4}")),
     # lectura actual del contador, y fecha
-    l_act = str_extract(value, pattern = "(?<=Lectura Actual: )\\d+") |> as.numeric(),
-    f_act = str_extract(value, pattern = "(\\d{2}/){2}\\d{4}(?=\\s+Con)") |> dmy(),
-    
-    consumo_m3 = xtr_cifra(value, patron = "\\d+$"),
-    precio = xtr_cifra(value, patron = "(?<=precio\\s{1,10})\\d,\\d+"),
-    total  = xtr_cifra(value, patron = "(?<=Total )(\\d\\.)?\\d{3},\\d{2}")
-    ) |> 
-  fill(c(precio, total), .direction = "up") |> 
-  drop_na() |> 
-  select(!value) |> 
+    l_act = as.numeric(str_extract(lineas, pattern = "(?<=Lectura Actual: )\\d+")),
+    f_act = dmy(str_extract(lineas, pattern = "(\\d{2}/){2}\\d{4}(?=\\s+Con)")),
+    consumo_m3 = xtr_cifra(lineas, patron = "\\d+$"),
+    precio = xtr_cifra(lineas, patron = "(?<=precio\\s{1,10})\\d,\\d+"),
+    total = xtr_cifra(lineas, patron = "(?<=Total )(\\d\\.)?\\d{3},\\d{2}")
+  ) |>
+  fill(c(precio, total), .direction = "up") |>
+  drop_na() |>
+  select(!value) |>
   arrange(f_act)
 
-balten_ds$bimensuales <-
-  select(balten_ds$todos, f_act:total) |> 
-  rename(fecha = f_act, total_eur = total)
+balten_ds$bimensuales <- balten_ds$todos |>
+  select(fecha = f_act, consumo_m3, precio, total_eur = total)
 
 save(balten_ds, file = here(dirs$pro, "datos_balten.RData"))
 
 # Limpio ------------------------------------------------------------------
 
-rm(xtr_cifra, balten_files, balten)
+rm(xtr_cifra, balten_files, balten_raw)
