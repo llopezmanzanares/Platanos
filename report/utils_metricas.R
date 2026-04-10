5dfca79a7149ba2ef625d4a73204b3df49560298
Luis Ovidio Lopez-Manzanares Fernandez <luislmf@gmail.com>
2026-04-10 15:35:12 +0100

    Métricas para el informe. Cálculo de ingresos y gastos anuales

---
+++ report/utils_metricas.R
@@ -0,0 +1,34 @@
+# report/utils_metricas.R
+
+# --- Ingresos y Gastos anuales
+
+ing_gas <-
+  left_join(
+    finca_eda$liq_total_aa |>
+      select(fecha, liquidaciones = total_eur_acum, kg = total_kg_acum) |>
+      mutate(aa = year(fecha), .keep = "unused"),
+    finca_eda$ooii_total_aa |>
+      select(aa, otros_ingresos),
+    by = "aa"
+  ) |>
+  left_join(
+    finca_eda$balten_total_aa |>
+      mutate(
+        aa = year(fecha_act),
+        agua = total_eur_acum / 2, .keep = "none"
+      ),
+    by = "aa"
+  ) |>
+  left_join(
+    finca_eda$oogg_total_aa |>
+      select(aa, otros_gastos = neto_acum),
+    by = "aa"
+  ) |>
+  mutate(
+    ingresos = liquidaciones + replace_na(otros_ingresos, 0),
+    gastos = replace_na(agua, 0) + replace_na(otros_gastos, 0),
+    beneficios = ingresos - gastos,
+    d_benef = beneficios / ingresos,
+    eur_kg = gastos / kg
+  ) |>
+  select(aa, ingresos, gastos, beneficios, d_benef, eur_kg)
